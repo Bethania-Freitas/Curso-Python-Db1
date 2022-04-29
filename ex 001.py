3f33789a8f1e9b325f67ec867757db2ad9be6cac
Bethania-Freitas <bethaniafreitas@yahoo.com.br>
2022-04-29 16:11:51 -0300

    atualizado

--- ex 001.py
+++ ex 001.py
@@ -1,4 +1,4 @@
-#Faça um Programa que peça dois números e imprima o maior deles
+#Faça um Programa que peça dois números e imprima o maior dele
 
 numero1 = int(input("Digite um numero: "))
 numero2 = int(input('Digite outro numero: '))
@@ -8,7 +8,6 @@ elif numero1 < numero2:
     print(f'O maior numero é {numero2}')
 else:
     print('Ambos os numeros são iguais')
-
 
 
 
